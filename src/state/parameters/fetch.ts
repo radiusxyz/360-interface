9ae6849e92b29eca674f9217b71ab981d9c5476a
hyemmie <lhm0710@naver.com>
2022-10-25 02:10:16 +0900

    seperate parameter fetch logic

---
+++ src/state/parameters/fetch.ts
@@ -0,0 +1,67 @@
+import localForage from 'localforage'
+
+import { VdfParam } from './reducer'
+
+export async function fetchVdfParam(callback: (res: boolean) => void): Promise<VdfParam> {
+  return await fetch('/parameters/vdf_zkp_parameter.data.bin', {
+    method: 'GET',
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      localForage.setItem('vdf_param', res)
+      callback(true)
+      return res
+    })
+}
+
+export async function fetchVdfSnarkParam(callback: (res: boolean) => void): Promise<string> {
+  return await fetch('/parameters/vdf_zkp_snark_parameter.data.bin', {
+    method: 'GET',
+  }).then(async (res) => {
+    const bytes = await res.arrayBuffer()
+    const uint8bytes = new Uint8Array(bytes)
+    const string = Buffer.from(uint8bytes).toString('hex')
+    localForage.setItem('vdf_snark_param', string)
+    callback(true)
+    return string
+  })
+}
+
+export async function fetchEncryptionParam(callback: (res: boolean) => void): Promise<string> {
+  return await fetch('/parameters/encryption_zkp_parameter.data.bin', {
+    method: 'GET',
+  }).then(async (res) => {
+    const bytes = await res.arrayBuffer()
+    const uint8bytes = new Uint8Array(bytes)
+    const string = Buffer.from(uint8bytes).toString('hex')
+    localForage.setItem('encryption_param', string)
+    callback(true)
+    return string
+  })
+}
+
+export async function fetchEncryptionProverKey(callback: (res: boolean) => void): Promise<string> {
+  return await fetch('/parameters/encryption_prover_key.data.bin', {
+    method: 'GET',
+  }).then(async (res) => {
+    const bytes = await res.arrayBuffer()
+    const uint8bytes = new Uint8Array(bytes)
+    const string = Buffer.from(uint8bytes).toString('hex')
+    localForage.setItem('encryption_prover_key', string)
+    callback(true)
+    return string
+  })
+}
+
+export async function fetchEncryptionVerifierData(callback: (res: boolean) => void): Promise<string> {
+  return await fetch('/parameters/encryption_verifier_data.data.bin', {
+    method: 'GET',
+  }).then(async (res) => {
+    const bytes = await res.arrayBuffer()
+    const uint8bytes = new Uint8Array(bytes)
+    const string = Buffer.from(uint8bytes).toString('hex')
+    localForage.setItem('encryption_verifier_data', string)
+    callback(true)
+    return string
+  })
+}
