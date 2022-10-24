381fa3f53ca7c717ee015c495bb4d2f0a3f29e9c
hyemmie <lhm0710@naver.com>
2022-10-24 12:14:16 +0900

    add wasm module

---
+++ src/wasm/vdf.ts
@@ -0,0 +1,29 @@
+import { VdfParam } from 'state/parameters/reducer'
+
+interface VdfResponse {
+  r1: string
+  r3: string
+  s1: string
+  s3: string
+  k: string
+  vdf_snark_proof: string
+  s2_string: string
+  s2_field_hex: string
+  commitment_hex: string
+}
+
+export async function getVdfProof(vdfParam: VdfParam, vdfSnarkParam: string): Promise<VdfResponse> {
+  const vdf = await import('wasm-vdf-zkp')
+  const data = await vdf
+    .get_vdf_proof(vdfParam, vdfSnarkParam)
+    .then((res) => {
+      console.log(res)
+      return res
+    })
+    .catch((error) => {
+      console.error(error)
+      return error
+    })
+
+  return data
+}
