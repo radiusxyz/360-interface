f828e094bdcaddc01477793ae42cbe8d5430d274
gylman <gylmanbm@gmail.com>
2023-06-08 12:53:43 +0900

    initial setup

---
+++ src/assets/v2/data.ts
@@ -0,0 +1,77 @@
+export type Token = {
+  title: string;
+  balance: number;
+  balanceInUSD: number;
+  description: string;
+};
+
+export type Tokens = Token[];
+
+export const tokens: Tokens = [
+  {
+    title: 'MATIC',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'DAI',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'WETH',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'WMATIC',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'WETH',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'DAI',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'WETH',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'BTC',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'ETH',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'WETH',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+  {
+    title: 'BTC',
+    description: 'Polygon Matic',
+    balance: 15578252.121,
+    balanceInUSD: 15584.15,
+  },
+];
