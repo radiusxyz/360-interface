f7a4b872493ff1dc96dc9510cb81e992a71a8907
gylman <gylmanbm@gmail.com>
2023-06-09 19:15:45 +0900

    upgrade dependencies, make error workarounds

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -26,7 +26,9 @@ interface Window {
   web3?: Record<string, unknown>
 }
 
-declare module 'content-hash' {
+// Commented out by Gylman
+
+/* declare module 'content-hash' {
   declare function decode(x: string): string
   declare function getCodec(x: string): string
 }
@@ -34,4 +36,9 @@ declare module 'content-hash' {
 declare module 'multihashes' {
   declare function decode(buff: Uint8Array): { code: number; name: string; length: number; digest: Uint8Array }
   declare function toB58String(hash: Uint8Array): string
-}
+} */
+
+declare module '*.png'
+declare module '*.svg'
+declare module '*.jpeg'
+declare module '*.jpg'
