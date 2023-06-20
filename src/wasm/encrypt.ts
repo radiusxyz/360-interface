da353df51902ea51e62706cfcb2bb3b80a9c9333
hyemmie <lhm0710@naver.com>
2023-06-20 15:50:22 +0900

    update radius wasm package

--- src/wasm/encrypt.ts
+++ src/wasm/encrypt.ts
@@ -21,7 +21,7 @@ export async function poseidonEncrypt(
   commitment: string,
   plainText: string
 ): Promise<EncryptResponseWithTxId> {
-  const poseidon = await import('wasm-encryptor-zkp')
+  const poseidon = await import('@radiusxyz/wasm-encryptor-zkp')
   const data = await poseidon
     .encrypt_threesixty_tx(tx_info, s2_string, s2_field_hex, commitment, plainText)
     .then((res) => {
